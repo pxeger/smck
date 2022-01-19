814322303a6d1ca911ea8074f822692f97318e4d
Patrick Reader <_@pxeger.com>
2022-01-19 08:14:42 +0000

    add raw_html

--- lib/options.rb
+++ lib/options.rb
@@ -3,6 +3,9 @@
 module Smck
   class Options
     def initialize
+      @allow_raw_html = false
     end
+
+    attr_accessor :allow_raw_html
   end
 end
--- lib/smck.rb
+++ lib/smck.rb
@@ -21,6 +21,14 @@ module Smck
     def str(data, parent)
       parent.add_child create_text_node data
     end
+
+    def raw_html(data, parent)
+      raise Error, 'raw_html is not allowed' unless @options.allow_raw_html
+
+      raise Error, "raw_html requires a string (found #{data.class})" unless data.is_a? String
+
+      parent.add_child data
+    end
   end
 
   # sanity check
